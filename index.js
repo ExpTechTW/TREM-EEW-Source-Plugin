d9dff2d2697b25aaa955d613d6ede312d5ec915f
Miyashooooo <cstrike.asia@gmail.com>
2024-12-08 14:15:22 +0800

    fix: show trem eew

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ class Plugin {
       const eewSource =
         JSON.parse(localStorage.getItem("eew-source-plugin")) || [];
       TREM.constant.EEW_AUTHOR = eewSource;
+      TREM.constant.SHOW_TREM_EEW = true;
       window.this_constant = TREM.constant.EEW_AUTHOR;
       logger.info(
         "Earthquake early warning source change success!",
